ae395259c2754dce9f688d846c19e09ad5af8e95
Juan Francisco Monge <juanfrancisco.monge@bbva.com>
2018-11-28 16:19:12 +0100

    [CRYP-582]: fixed validation of getCode

--- src/controllers/ethereum.ts
+++ src/controllers/ethereum.ts
@@ -232,7 +232,8 @@ export const _reactToNewTransaction = async (
 
         try {
 
-          const isSmartContractRelated = txBody.to === null || await web3I.eth.getCode(txBody.to) !== '0x0';
+          const code = await web3I.eth.getCode(txBody.to);
+          const isSmartContractRelated = txBody.to === null ||  (code !== '0x0' && code !== '0x');
           const sendTx = !onlyTransfers || !isSmartContractRelated;
 
           if (sendTx) {
