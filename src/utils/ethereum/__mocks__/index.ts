54ea0b875a94fb35eadc4ee76c66f85d243580a1
Juan Ferrer <juan.ferrer2@bbva.com>
2018-11-29 16:09:32 +0100

    [CRYP-477]Fixed healthcheck tests

--- src/utils/ethereum/__mocks__/index.ts
+++ src/utils/ethereum/__mocks__/index.ts
@@ -1,38 +1,43 @@
 // tslint:disable-next-line:variable-name
 export const __mockWeb3__ = {
-    eth: {
-        getBlock: jest.fn().mockImplementation(() => {
-            const promise = Promise.resolve('whatever');
-            (promise as any).on = jest.fn().mockReturnValue(promise);
-            return promise;
-        }),
-        getCode: jest.fn().mockImplementation(() => {
-            const promise = Promise.resolve('whatever');
-            (promise as any).on = jest.fn().mockReturnValue(promise);
-            return promise;
-        }),
-        sendSignedTransaction: jest.fn().mockImplementation(() => {
-            const promise = Promise.resolve('whatever');
-            (promise as any).on = jest.fn().mockReturnValue(promise);
-            return promise;
-        }),
-        sendTransaction: jest.fn().mockImplementation(() => {
-            const promise = Promise.resolve('whatever');
-            (promise as any).on = jest.fn().mockReturnValue(promise);
-            return promise;
-        }),
-        subscribe: jest.fn().mockImplementation(() => {
-            const promise = Promise.resolve('whatever');
-            (promise as any).on = jest.fn().mockReturnValue(promise);
-            return promise;
-        }),
-        Contract: jest.fn().mockImplementation(() => {
-            const promise = Promise.resolve('whatever');
-            (promise as any).on = jest.fn().mockReturnValue(promise);
-            return promise;
-        }),
+  eth: {
+    getBlock: jest.fn().mockImplementation(() => {
+      const promise = Promise.resolve('whatever');
+      (promise as any).on = jest.fn().mockReturnValue(promise);
+      return promise;
+    }),
+    getCode: jest.fn().mockImplementation(() => {
+      const promise = Promise.resolve('whatever');
+      (promise as any).on = jest.fn().mockReturnValue(promise);
+      return promise;
+    }),
+    sendSignedTransaction: jest.fn().mockImplementation(() => {
+      const promise = Promise.resolve('whatever');
+      (promise as any).on = jest.fn().mockReturnValue(promise);
+      return promise;
+    }),
+    sendTransaction: jest.fn().mockImplementation(() => {
+      const promise = Promise.resolve('whatever');
+      (promise as any).on = jest.fn().mockReturnValue(promise);
+      return promise;
+    }),
+    subscribe: jest.fn().mockImplementation(() => {
+      const promise = Promise.resolve('whatever');
+      (promise as any).on = jest.fn().mockReturnValue(promise);
+      return promise;
+    }),
+    Contract: jest.fn().mockImplementation(() => {
+      const promise = Promise.resolve('whatever');
+      (promise as any).on = jest.fn().mockReturnValue(promise);
+      return promise;
+    }),
+    net: {
+      isListening: jest.fn().mockImplementation(() => {
+        return Promise.resolve(true);
+      }),
     },
-  };
+  },
+};
 
 export const getWeb3 = jest.fn().mockResolvedValue(__mockWeb3__);
 
